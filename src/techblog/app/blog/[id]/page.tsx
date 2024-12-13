5c1615e411bf8b2cea63edafb663edc7e8b90829
Ryn0suke <xincunlongzhijie@gmail.com>
2024-12-14 02:26:16 +0900

    シンタックスハイライトの適用

--- src/techblog/app/blog/[id]/page.tsx
+++ src/techblog/app/blog/[id]/page.tsx
@@ -1,18 +1,44 @@
 import { getPagesFromDatabase } from "@/app/notion_api/access";
+import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default async function Page({
-    params,
-  }: {
-    params: Promise<{ id: string }>
-  }) {
-    const id = (await params).id;
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  const md = await getPagesFromDatabase(id);
 
-    const md = getPagesFromDatabase(id);
+  return (
+    <>
+      {/* Markdownの生データを表示（デバッグ用） */}
+      {/* <div>{md}</div> */}
 
-    return(
-        <>
-            <div>My Post: {id}</div>
-            <div>{md}</div>
-        </>
-    )
-  }
+      {/* Markdownをレンダリング */}
+      <Markdown
+        children={md}
+        components={{
+          code(props) {
+            const { children, className, node, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                children={String(children).replace(/\n$/, "")}
+                language={match[1]}
+                style={vscDarkPlus}
+              />
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    </>
+  );
+}
--- src/techblog/components/blogCard.tsx
+++ src/techblog/components/blogCard.tsx
@@ -3,7 +3,7 @@ import { NotionProperties } from "@/app/types/notion";
 export default function BlogCard({ property }: { property: NotionProperties }) {
   return (
     <div className="max-w-sm h-80 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between">
-      <a href="#">
+      <a href={`/blog/${property.id}`}>
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-2">
           {property.title}
         </h5>
@@ -15,7 +15,7 @@ export default function BlogCard({ property }: { property: NotionProperties }) {
         作成日：{property.createdate}
       </p>
       <a
-        href="#"
+        href={`/blog/${property.id}`}
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-red dark:hover:bg-red-700 dark:focus:ring-red-800"
       >
         Read more
