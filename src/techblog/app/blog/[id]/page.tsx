4c4cdcde892777035276ae645ad82a7947f939be
Ryn0suke <xincunlongzhijie@gmail.com>
2024-12-15 03:04:54 +0000

    ブログページのレイアウトを変更

--- src/techblog/app/blog/[id]/page.tsx
+++ src/techblog/app/blog/[id]/page.tsx
@@ -14,33 +14,36 @@ export default async function Page({
 
   return (
     <>
-      <h1>{pageInfo.title}</h1>
-      <h3>投稿日：{pageInfo.date}</h3>
-      <h3>著者：{pageInfo.author}</h3>
+      <div className='max-w-4xl mx-auto p-6 bg-gray-100 rounded-lg shadow-lg'>
+        <h1>{pageInfo.title}</h1>
+        <h3>投稿日：{pageInfo.date}</h3>
+        <h3>著者：{pageInfo.author}</h3>
 
-      <Markdown
-        components={{
-          code({ children, className, ...rest }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return match ? (
-              <SyntaxHighlighter
-                {...rest}
-                PreTag="div"
-                language={match[1]}
-                style={vscDarkPlus}
-              >
-                {String(children).replace(/\n$/, '')}
-              </SyntaxHighlighter>
-            ) : (
-              <code {...rest} className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}
-      >
-        {md}
-      </Markdown>
+        <Markdown
+          components={{
+            code({ children, className, ...rest }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return match ? (
+                <SyntaxHighlighter
+                  {...rest}
+                  PreTag='div'
+                  language={match[1]}
+                  style={vscDarkPlus}
+                >
+                  {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
+              ) : (
+                <code {...rest} className={className}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        >
+          {md}
+        </Markdown>
+      </div>
+
     </>
   );
 }
